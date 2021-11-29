f7d485eb71f61fdd1ea4a4837ad46c55ce63f8ed
Rodrigo Pequeno <rodrigo.pequeno6@gmail.com>
2021-11-28 22:34:21 -0300

    fix(collect/database): adding typing of data saved in the database

--- lib/features/collect/data/datasources/collect_local_data_source.dart
+++ lib/features/collect/data/datasources/collect_local_data_source.dart
@@ -21,11 +21,14 @@ class CollectLocalDataSourceImpl implements CollectLocalDataSource {
   Future<void> saveCacheNewCollect({
     required CollectModel collectModel,
   }) async {
-    final values =
-        await database.get(boxName: boxCacheCollect, key: boxCacheCollect);
-    final cacheCollect = List<CollectModel>.from(values);
+    final values = await database.get<String, List>(
+      boxName: boxCacheCollect,
+      key: boxCacheCollect,
+      defaultValue: <CollectModel>[],
+    );
+    final cacheCollect = List<CollectModel>.from(values ?? <CollectModel>[]);
     cacheCollect.add(collectModel);
-    await database.put(
+    await database.put<String, List>(
       boxName: boxCacheCollect,
       key: boxCacheCollect,
       value: cacheCollect,
@@ -36,11 +39,14 @@ class CollectLocalDataSourceImpl implements CollectLocalDataSource {
   Future<void> saveUnsentCollect({
     required CollectModel collectModel,
   }) async {
-    final values =
-        await database.get(boxName: boxUnsentCollect, key: boxUnsentCollect);
-    final unsentCollect = List<CollectModel>.from(values);
+    final values = await database.get<String, List>(
+      boxName: boxUnsentCollect,
+      key: boxUnsentCollect,
+      defaultValue: <CollectModel>[],
+    );
+    final unsentCollect = List<CollectModel>.from(values ?? <CollectModel>[]);
     unsentCollect.add(collectModel);
-    await database.put(
+    await database.put<String, List>(
       boxName: boxUnsentCollect,
       key: boxUnsentCollect,
       value: unsentCollect,
